17f43e26da83efeee133959a70f15ed6790ae902
sojibhasan9878 <sojibhasan9878@gmail.com>
2024-03-02 00:23:33 +0600

    all phones are dynamically added and search btn done

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const loadData = async (search) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`);
+
+    const data =await res.json();
+    const phones = data.data;
+    displayPhones(phones)
+}
+
+loadData()
+
+
+const displayPhones = phones =>{
+    const phoneDiv = document.getElementById("phone-card-container");
+    phoneDiv.innerText = "";
+        phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement("div");
+        phoneCard.classList = `card bg-base-100 shadow-xl`
+        phoneCard.innerHTML= `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        `
+        phoneDiv.appendChild(phoneCard)
+    });
+}
+
+const hanledSearch = () =>{
+    console.log("I am clicked");
+    const searchFild = document.getElementById("search-fild");
+    const searchText = searchFild.value;
+    console.log(searchText);
+    loadData(searchText);
+}
